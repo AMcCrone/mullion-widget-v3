0243d027b3dbc39f877792538cf517f163623125
AMcCrone <AMcCrone@ThorntonTomasetti.com>
2025-03-14 20:29:34 +0000

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+def set_page_config():
+    st.set_page_config(page_title="Section Design Tool", layout="wide")
+
+# Define TT Colours
+TT_Orange = "rgb(211,69,29)"
+TT_Olive = "rgb(139,144,100)"
+TT_LightBlue = "rgb(136,219,223)"
+TT_MidBlue = "rgb(0,163,173)"
+TT_DarkBlue = "rgb(0,48,60)"
+TT_Grey = "rgb(99,102,105)"
+TT_Purple = "rgb(128,0,128)"  # For custom sections
+
+# Material properties
+material_props = {
+    "Aluminium": {"fy": 160, "E": 70000},
+    "Steel": {"fy": 355, "E": 210000}
+}
+
+# Constant: Barrier Length (mm)
+BARRIER_LENGTH = 1100
